8084cd5938f049ce06ca0c6abe19a83a4468f457
Imtiaz Epu <imtiazepu@programmer.net>
2019-01-29 05:31:45 +0600

    categories list added in menu

--- resources/views/frontend/partials/_header.blade.php
+++ resources/views/frontend/partials/_header.blade.php
@@ -3,8 +3,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 col-md-7 py-4">
-                    <h4 class="text-white">About</h4>
-                    <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+                    <h4 class="text-white">Categories</h4>
+                    <ul class="list-unstyled">
+                        @foreach($categories as $category)
+                            <li>
+                                <a href="{{$category->slug}}" class="text-white">{{ $category->name }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
                 <div class="col-sm-4 offset-md-1 py-4">
                     <h4 class="text-white">Menu</h4>
