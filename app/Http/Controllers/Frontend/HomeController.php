f34e05f69fe0ed4474ae588069180d71a167f57f
Imtiaz Epu <imtiazepu@programmer.net>
2019-01-29 06:35:31 +0600

    home controller added

---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+
+class HomeController extends Controller
+{
+    public function showHomePage()
+    {
+        $data = [];
+        $data['products'] = Product::select(['id','title', 'slug', 'price', 'sale_price'])
+            ->where('active', 1)
+            ->paginate(9);
+        return view('frontend.home',$data);
+
+    }
+}
