cd7516be26beb9060559af6f098e08146ab8251c
Imtiaz Epu <imtiazepu@programmer.net>
2019-01-31 01:55:03 +0600

    Post method added for 'add to cart'

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::group(['namespace' => 'Frontend'], function () {
     Route::get('/', 'HomeController@showHomePage')->name('frontend.home');
     Route::get('/product/{slug}', 'ProductController@showDetails')->name('product.details');
     Route::get('/cart', 'CartController@showCart')->name('cart.show');
+    Route::post('/cart/{id}', 'CartController@addToCart')->name('cart.add');
 });
