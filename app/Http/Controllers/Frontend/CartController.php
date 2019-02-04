d8aca499e9e05b8a6a2ed269020f8552ff9d61f8
Imtiaz Epu <imtiazepu@programmer.net>
2019-02-05 02:28:55 +0600

    order process added

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class CartController extends Controller
 {
+
     public function showCart()
     {
         $data = [];
@@ -75,6 +78,47 @@ class CartController extends Controller
         $data['cart'] = session()->has('cart') ? session()->get('cart') : [];
         $data['total'] = array_sum(array_column($data['cart'], 'total_price'));
         return view('frontend.checkout', $data);
+    }
+
+    public function processOrder()
+    {
+        $validator = Validator::make(request()->all(),[
+            'customer_name' => 'required',
+            'customer_phone_number' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'postal_code' => 'required',
+        ]);
+        if ($validator->fails()){
+            return redirect()->back()->withErrors($validator);
+        }
+        $cart = session()->has('cart') ? session()->get('cart') : [];
+        $total = array_sum(array_column($cart, 'total_price'));
+
+        $order = Order::create([
+            'user_id' => auth()->user()->id,
+            'customer_name' => request()->input('customer_name'),
+            'customer_phone_number' => request()->input('customer_phone_number'),
+            'address' => request()->input('address'),
+            'city' => request()->input('city'),
+            'postal_code' => request()->input('postal_code'),
+            'total_amount' => $total,
+            'paid_amount' => $total,
+            'payment_details' => 'Cash on delivery',
+        ]);
+
+        foreach ($cart as $product_id => $product) {
+
+            $order->products()->create([
+                'product_id' => $product_id,
+                'quantity' => $product['quantity'],
+                'price' => $product['total_price'],
+            ]);
+        }
+            session()->forget(['total', 'cart']);
+            session()->flash('message', 'Order successfully placed.');
+
+            return redirect('/');
     }
 
 }
