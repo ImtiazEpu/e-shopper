9f169ec9b9942af45e9eb7ad68f258d47582f50f
Imtiaz Epu <imtiazepu@programmer.net>
2019-08-05 23:42:56 +0600

    query optimization

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function showHomePage()
     {
         $data = [];
-        $data['products'] = Product::select(['id','title', 'slug', 'price', 'sale_price'])
+        $data['products'] = Product::with('media')->select(['id','title', 'slug', 'price', 'sale_price'])
             ->where('active', 1)
             ->paginate(9);
         $data['cart'] = session()->has('cart') ? session()->get('cart') : [];
